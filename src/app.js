8bb12b3355ccc7bae88d50a953a9b85a24c9634b
Chris Khedoo <chris@Chriss-MBP.lan>
2022-09-14 19:28:00 -0400

    attempted to fix public folder visibility issues

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(helmet());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 // domain config
 app.get('/', (req, res) => {
