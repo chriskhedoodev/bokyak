a84f999194027848eaffbc5e7429dbf796fc2e92
Chris Khedoo <chris@Chriss-MBP.lan>
2022-09-14 19:29:28 -0400

    reverted previous change

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(helmet());
-app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // domain config
 app.get('/', (req, res) => {
