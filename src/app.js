c33020a600d72278fddf55ad9843110a4a8bb33b
Chris Khedoo <chris@Chriss-MBP.lan>
2022-09-14 18:55:42 -0400

    added more infrastructure stuff

--- src/app.js
+++ src/app.js
@@ -1,10 +1,27 @@
+// system imports
 const dotenv = require('dotenv');
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const helmet = require('helmet');
+const mongoose = require('mongoose');
 
+// environment config
 dotenv.config();
 
+// server config
 const app = express();
-const port = process.env.PORT;
-app.listen(port, () => {
-    console.log(`Server has started running on port ${port}....`);
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(helmet());
+
+// connect to database and start server
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true }, () => {
+    console.log('Connected to database.');
+
+    let port = process.env.PORT || 3000;
+    app.listen(port, () => {
+        console.log(`Server has started on port ${port}`);
+    });
 });
